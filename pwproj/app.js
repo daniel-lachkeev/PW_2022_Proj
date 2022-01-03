2deaa4e4c00961e741a208e406751115719d8a2d
Daniel Lachkeev <74510099+daniel-lachkeev@users.noreply.github.com>
2022-01-03 19:17:55 +0000

    Add files via upload

---
+++ pwproj/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const app = express();
+
+app.set('view engine', 'pug')
+app.set('views', './src/views')
+
+app.get('/', (req, res) =>
+    res.render('index', {
+        carros: [ //array de carros
+            "fiesta",
+            "gol",
+            "uno"
+        ]
+    })
+);
+
+app.use("/images", express.static(path.join(__dirname, 'images')));
+
+app.listen(3000, () =>
+    console.log('Servidor iniciado na porta 3000')
+);
